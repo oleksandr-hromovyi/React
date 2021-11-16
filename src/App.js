95dc04cf0528012b8c35286af8199b9331e5868c
oleksandr-hromovyi <alexander.gromovoj@gmail.com>
2021-11-16 15:43:51 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './news.json';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	const getFormattedDate = (dateStr) => {
+		const date = new Date(dateStr.replaceAll(` `, ``));
+		return date.toLocaleString();
+	}
+	
+	return (
+     <div>
+       {data.map((item, index) =>
+        	<div className = "block" key={`${item.id}${index}`}>
+        		<span key={item.id} className="block-id"><b>Id:</b> {item.id}</span>
+        		<hr/>
+        		<h2 key={`${item.id} ${item.title}`} 
+        			className={item.isSpecial === true ? "block-title_specialnews" : "block-title" } 
+        		>{item.title}</h2>
+         		<div className="block-content" dangerouslySetInnerHTML={{
+       				__html: item.content}}/>     
+        		<span key={`${item.id} ${item.dateCreated}`}>{getFormattedDate(item.dateCreated)}</span>
+        		<ul>
+        		{item.categories.map((item) => 
+		        	<li data-id={item.id}>{item.name}</li>)}
+				</ul>
+				{item.link !== null && <a href={item.link} class="content-link">link</a> }
+				{item.photo && <img src={item.photo} alt="" height="200px" width="200px"/>}
+				{item.author && <p><em>{item.author}</em></p>}
+          	</div> 
+        	)}
+     </div>
   );
 }
 
